269c3606f194dc8da4246aed381aed7a555ec55f
Lorenzo Bretto <lorenzo.bretto@enyr.eu>
2025-11-25 14:51:58 +0000

    added py.typed

--- src/modelmirror/class_provider/class_register.py
+++ src/modelmirror/class_provider/class_register.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 from modelmirror.class_provider.class_reference import ClassReference
 
 
 class ClassRegister:
     reference: ClassReference
 
-    def __init_subclass__(cls, reference: ClassReference, **kwargs):
+    def __init_subclass__(cls, *, reference: ClassReference, **kwargs: Any) -> None:
         super().__init_subclass__(**kwargs)
         cls.reference = reference
